df9672148a57eb3aee9f9f5e66acd2d17fd01e2b
hathateth <albertoslavica19@gmail.com>
2022-11-17 14:02:19 +0100

    Save

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
+
+
 const Header = () => {
+   
+
   return (
     // Sinistra
     <>
       {/* Sinistra */}
       <div className=" columns-2">
         <h1 className="flex justify-center">Test</h1>
-        <h1 className="flex justify-center">Ciaooo!</h1>
       </div>
       {/* Destra */}
-      
-      
+      <div>
+          
+      </div>
     </>
   );
 };
