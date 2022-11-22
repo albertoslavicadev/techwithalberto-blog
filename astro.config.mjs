cdeb67faa71f67451108d214f422f930128dd4e9
hathateth <albertoslavica19@gmail.com>
2022-11-22 12:03:15 +0100

    Added example blog post and list

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,4 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: node({
-    mode: 'middleware'
-  }),
 });
--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -15,4 +15,4 @@ const Cookie = () => {
   );
 };
 
-export default Cookie;
+export default Cookie
